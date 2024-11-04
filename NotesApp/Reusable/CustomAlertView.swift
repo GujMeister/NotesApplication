d49737d37f82d45594b814d56b7f80ecccef7dca
GujMeister <90295019+GujMeister@users.noreply.github.com>
2024-11-04 17:43:20 +0400

    Fixing unreasonably long UI Setup function

--- NotesApp/Reusable/CustomAlertView.swift
+++ NotesApp/Reusable/CustomAlertView.swift
@@ -9,13 +9,63 @@ import UIKit
 
 final class CustomAlertView: UIView {
     // MARK: - Properties
-    private let infoImageView = UIImageView()
-    private let messageLabel = UILabel()
-    private let cancelButton = UIButton(type: .system)
-    private let deleteButton = UIButton(type: .system)
-    
     var onCancel: (() -> Void)?
     var onDelete: (() -> Void)?
+    
+    private lazy var containerView: UIView = {
+        let view = UIView()
+        view.backgroundColor = UIColor(hex: "#252525")
+        view.layer.cornerRadius = 12
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+    
+    private lazy var infoImageView: UIImageView = {
+        let imageView = UIImageView()
+        let symbolConfig = UIImage.SymbolConfiguration(pointSize: 50, weight: .regular)
+        imageView.image = UIImage(systemName: "info.circle.fill", withConfiguration: symbolConfig)
+        imageView.tintColor = UIColor(hex: "#606060")
+        imageView.contentMode = .scaleAspectFit
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        return imageView
+    }()
+    
+    private lazy var messageLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Are you sure you want to\ndelete this note?"
+        label.font = UIFont(name: "Nunito-Regular", size: 20)
+        label.textColor = UIColor(hex: "#CFCFCF")
+        label.numberOfLines = 0
+        label.textAlignment = .center
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    private lazy var cancelButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("Cancel", for: .normal)
+        button.setTitleColor(.white, for: .normal)
+        button.backgroundColor = UIColor(hex: "#30BE71")
+        button.layer.cornerRadius = 6
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.addAction(UIAction(handler: { [weak self] _ in
+            self?.cancelButtonTapped()
+        }), for: .touchUpInside)
+        return button
+    }()
+    
+    private lazy var deleteButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("Delete", for: .normal)
+        button.setTitleColor(.white, for: .normal)
+        button.backgroundColor = UIColor(hex: "#FF0000")
+        button.layer.cornerRadius = 6
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.addAction(UIAction(handler: { [weak self] _ in
+            self?.deleteButtonTapped()
+        }), for: .touchUpInside)
+        return button
+    }()
     
     // MARK: - Initializers
     override init(frame: CGRect) {
@@ -30,71 +80,38 @@ final class CustomAlertView: UIView {
     // MARK: - Setup View
     private func setupView() {
         backgroundColor = UIColor.black.withAlphaComponent(0.6)
-        
-        let container = UIView()
-        container.backgroundColor = UIColor(hex: "#252525")
-        container.layer.cornerRadius = 12
-        container.translatesAutoresizingMaskIntoConstraints = false
-        addSubview(container)
-        
-        let symbolConfig = UIImage.SymbolConfiguration(pointSize: 50, weight: .regular)
-        let infoImage = UIImage(systemName: "info.circle.fill", withConfiguration: symbolConfig)
-        infoImageView.image = infoImage
-        infoImageView.tintColor = UIColor(hex: "#606060")
-        infoImageView.contentMode = .scaleAspectFit
-        infoImageView.translatesAutoresizingMaskIntoConstraints = false
-        container.addSubview(infoImageView)
-        
-        messageLabel.text = "Are you sure you want to\ndelete this note?"
-        messageLabel.font = UIFont(name: "Nunito-Regular", size: 20)
-        messageLabel.textColor = UIColor(hex: "#CFCFCF")
-        messageLabel.numberOfLines = 0
-        messageLabel.textAlignment = .center
-        messageLabel.translatesAutoresizingMaskIntoConstraints = false
-        container.addSubview(messageLabel)
-        
-        cancelButton.setTitle("Cancel", for: .normal)
-        cancelButton.setTitleColor(.white, for: .normal)
-        cancelButton.backgroundColor = UIColor(hex: "#30BE71")
-        cancelButton.layer.cornerRadius = 6
-        cancelButton.translatesAutoresizingMaskIntoConstraints = false
-        cancelButton.addAction(UIAction(handler: { [weak self] _ in
-            self?.cancelButtonTapped()
-        }), for: .touchUpInside)
-        container.addSubview(cancelButton)
-        
-        deleteButton.setTitle("Delete", for: .normal)
-        deleteButton.setTitleColor(.white, for: .normal)
-        deleteButton.backgroundColor = UIColor(hex: "#FF0000")
-        deleteButton.layer.cornerRadius = 6
-        deleteButton.translatesAutoresizingMaskIntoConstraints = false
-        deleteButton.addAction(UIAction(handler: { [weak self] _ in
-            self?.deleteButtonTapped()
-        }), for: .touchUpInside)
-        container.addSubview(deleteButton)
+        setupLayout()
+    }
+    
+    private func setupLayout() {
+        addSubview(containerView)
+        containerView.addSubview(infoImageView)
+        containerView.addSubview(messageLabel)
+        containerView.addSubview(cancelButton)
+        containerView.addSubview(deleteButton)
         
         NSLayoutConstraint.activate([
-            container.centerXAnchor.constraint(equalTo: centerXAnchor),
-            container.centerYAnchor.constraint(equalTo: centerYAnchor),
-            container.widthAnchor.constraint(equalToConstant: 330),
+            containerView.centerXAnchor.constraint(equalTo: centerXAnchor),
+            containerView.centerYAnchor.constraint(equalTo: centerYAnchor),
+            containerView.widthAnchor.constraint(equalToConstant: 330),
             
-            infoImageView.topAnchor.constraint(equalTo: container.topAnchor, constant: 20),
-            infoImageView.centerXAnchor.constraint(equalTo: container.centerXAnchor),
+            infoImageView.topAnchor.constraint(equalTo: containerView.topAnchor, constant: 20),
+            infoImageView.centerXAnchor.constraint(equalTo: containerView.centerXAnchor),
             infoImageView.heightAnchor.constraint(equalToConstant: UIScreen.main.bounds.height * 0.0323),
             
             messageLabel.topAnchor.constraint(equalTo: infoImageView.bottomAnchor, constant: 10),
-            messageLabel.leadingAnchor.constraint(equalTo: container.leadingAnchor, constant: 20),
-            messageLabel.trailingAnchor.constraint(equalTo: container.trailingAnchor, constant: -20),
+            messageLabel.leadingAnchor.constraint(equalTo: containerView.leadingAnchor, constant: 20),
+            messageLabel.trailingAnchor.constraint(equalTo: containerView.trailingAnchor, constant: -20),
             
             cancelButton.topAnchor.constraint(equalTo: messageLabel.bottomAnchor, constant: 20),
-            cancelButton.leadingAnchor.constraint(equalTo: container.leadingAnchor, constant: 20),
-            cancelButton.bottomAnchor.constraint(equalTo: container.bottomAnchor, constant: -20),
+            cancelButton.leadingAnchor.constraint(equalTo: containerView.leadingAnchor, constant: 20),
+            cancelButton.bottomAnchor.constraint(equalTo: containerView.bottomAnchor, constant: -20),
             cancelButton.heightAnchor.constraint(equalToConstant: 40),
             cancelButton.widthAnchor.constraint(equalToConstant: UIScreen.main.bounds.width * 0.27),
             
             deleteButton.topAnchor.constraint(equalTo: messageLabel.bottomAnchor, constant: 20),
-            deleteButton.trailingAnchor.constraint(equalTo: container.trailingAnchor, constant: -20),
-            deleteButton.bottomAnchor.constraint(equalTo: container.bottomAnchor, constant: -20),
+            deleteButton.trailingAnchor.constraint(equalTo: containerView.trailingAnchor, constant: -20),
+            deleteButton.bottomAnchor.constraint(equalTo: containerView.bottomAnchor, constant: -20),
             deleteButton.heightAnchor.constraint(equalToConstant: 40),
             deleteButton.widthAnchor.constraint(equalToConstant: UIScreen.main.bounds.width * 0.27)
         ])
